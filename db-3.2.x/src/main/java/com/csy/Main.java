a8a9a6baf607a689b75d13ea6dac929aedadd3e7
ChenSy <chensy@cloudyigou.com>
2023-02-26 20:54:12 +0800

    初次提交

---
+++ db-3.2.x/src/main/java/com/csy/Main.java
@@ -0,0 +1,12 @@
+package com.csy;
+
+/**
+ * @Author chenSy
+ * @Date $YEAR/$MONTH/$DAY $HOUR:$MINUTE
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
